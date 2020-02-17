e069a342cd4f6a809440b7a19aebcced140f7c95
Shinnn <vincentqin2016@gmail.com>
2020-02-17 13:25:46 +0100

    new heuristic updated

--- searchclient_python/searchclient/heuristic.py
+++ searchclient_python/searchclient/heuristic.py
@@ -7,38 +7,39 @@ import sys
 
 class Heuristic(metaclass=ABCMeta):
     def __init__(self, initial_state: 'State',goals):
-        self.boxes = initial_state.boxes
         self.goals = goals
     
-    def is_there_immediate_neighbour(self,goal, row, col):
+    def is_there_immediate_neighbour(self,state,goal, row, col):
         if row+1 < State.MAX_ROW: #we are still on the board
-            box_test= self.boxes[row+1][col]
+            box_test= state.boxes[row+1][col]
             goal_test= self.goals[row+1][col]
             if (box_test is not None) and (goal == box_test.lower()) and (goal_test is None or goal_test != box_test.lower()):
                 return True
         if row-1 >= 0: #we are still on the board
-            box_test= self.boxes[row-1][col]
+            box_test= state.boxes[row-1][col]
             goal_test= self.goals[row-1][col]
             if (box_test is not None) and (goal == box_test.lower()) and (goal_test is None or goal_test != box_test.lower()):
                 return True
         if col+1 < State.MAX_COL: #we are still on the board
-            box_test= self.boxes[row][col+1]
+            box_test= state.boxes[row][col+1]
             goal_test= self.goals[row][col+1]
             if (box_test is not None) and (goal == box_test.lower()) and (goal_test is None or goal_test != box_test.lower()):
                 return True
         if col-1 >= 0: #we are still on the board
-            box_test= self.boxes[row][col-1]
+            box_test= state.boxes[row][col-1]
             goal_test= self.goals[row][col-1]
             if (box_test is not None) and (goal == box_test.lower()) and (goal_test is None or goal_test != box_test.lower()):
                 return True
         return False
 
-    def GetDistance(self,goal,queue,goal_row,goal_col):
+    def GetDistance(self,state,goal,queue,goal_row,goal_col):
         p = queue.popleft()
         row = p[0]
         col = p[1]
-        if self.is_there_immediate_neighbour(goal,row,col):
-            return math.sqrt((goal_row-row)**2+(goal_col/col)**2)
+        if self.is_there_immediate_neighbour(state,goal,row,col):
+            print("d number:",math.sqrt((goal_row-row)**2+(goal_col-col)**2), file=sys.stderr, flush=True)
+            return math.sqrt((goal_row-row)**2+(goal_col-col)**2)
+             
         else:
             if row+1 < State.MAX_ROW:
                 queue.append([row+1,col])
@@ -48,7 +49,7 @@ class Heuristic(metaclass=ABCMeta):
                 queue.append([row,col+1])
             if col-1 >= 0:
                 queue.append([row,col-1])
-            self.GetDistance(goal,queue,goal_row,goal_col)
+        return   self.GetDistance(state,goal,queue,goal_row,goal_col)
 
     def agent_to_distance(self,state,r,c):
         a_row = state.agent_row
@@ -62,21 +63,23 @@ class Heuristic(metaclass=ABCMeta):
         for row in range(State.MAX_ROW):
             for col in range(State.MAX_COL):
                 goal = self.goals[row][col]
-                box = self.boxes[row][col]
+                box = state.boxes[row][col]
                 if goal is not None:
                     if box is not None and goal == box.lower():
-                        r -= 100   # We might want to increase the weight of these
+                        r -= 5   # We might want to increase the weight of these
                     else:  # in goal but with no matching box.
                         queue = deque()
                         queue.append([row,col])
-                        print("goal:  ", goal, file=sys.stderr, flush=True)
-                        print("queue:  ", queue, file=sys.stderr, flush=True)
-                        print("row:  ", row, file=sys.stderr, flush=True)
-                        print("col:  ", col, file=sys.stderr, flush=True)
-                        print("agent:  {},{}".format(state.agent_row, state.agent_col), file=sys.stderr, flush=True)
+                      #  print("goal:  ", goal, file=sys.stderr, flush=True)
+                      #  print("queue:  ", queue, file=sys.stderr, flush=True)
+                      #  print("row:  ", row, file=sys.stderr, flush=True)
+                      #  print("col:  ", col, file=sys.stderr, flush=True)
+                      #  print("agent:  {},{}".format(state.agent_row, state.agent_col), file=sys.stderr, flush=True)
                         d = self.agent_to_distance(state,row,col)
-                        r += self.GetDistance(goal,queue,row,col)
-                        r = r * d
+                        d2 = self.GetDistance(state,goal,queue,row,col)
+                        print("return of get distance: ",d2, file=sys.stderr, flush=True)
+                        r =r + d2
+                        r = r + d
                         print("R:  ", r, file=sys.stderr, flush=True)
 
         return r                                
